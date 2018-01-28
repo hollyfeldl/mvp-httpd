451646369bdfd81fef317f3f45fee8eee211435d
Mark <hollyfeldl@gmail.com>
2018-01-27 17:14:42 -0800

    Expanded person with UUID and email
     Changes to be committed:
    	modified:   person/person.go
    	modified:   person/person_test.go

--- person/person.go
+++ person/person.go
@@ -3,21 +3,28 @@ package person
 // create a structure and methods to maintain an object for the cards
 
 type Person struct {
+	UID			string
+	EMail		string
     GivenName   string
     URLList     map[string]map[string]string
 }
 
-func NewPerson(curGivenName string) *Person {
+// function to create a new person
+func NewPerson(curUID string, curEMail string, curGivenName string ) *Person {
     p := new(Person)
+    p.UID = curUID
+    p.EMail = curEMail
     p.GivenName = curGivenName
     p.URLList = make(map[string]map[string]string)
     return p
 }
 
+// add a group of URL links 
 func (p *Person) NewPersonURLGroup(curGroupName string) {
     p.URLList[curGroupName] = make(map[string]string)
 }
 
+// add a URL to a group
 func (p *Person) AddPersonURL(curGroupName string, curURLLabel string, curURLHREF string) {
     p.URLList[curGroupName][curURLLabel] = curURLHREF
 }
--- person/person_test.go
+++ person/person_test.go
@@ -9,13 +9,16 @@ func TestPopulatePerson (t *testing.T) {
 
 	// A test for the person struct and generator functions
 
-    curPerson := NewPerson("Prsn")
+    curPerson := NewPerson("1234", "prsn@gmail.com", "Prsn")
     curPerson.NewPersonURLGroup("Grp")
     curPerson.AddPersonURL("Grp", "url1","http://url1.com/")
     curPerson.AddPersonURL("Grp", "url2","http://url2.com/")
     fmt.Println(curPerson)
 
-    // Output:
-    // &{Prsn map[Grp:map[url1:http://url1.com/ url2:http://url2.com/]]}
+    expectOutput := "&{1234 prsn@gmail.com Prsn map[Grp:map[url1:http://url1.com/ url2:http://url2.com/]]}"
+
+    if curOutput := fmt.Sprint(curPerson); curOutput != expectOutput {
+        t.Errorf("Expected '%s' but got '%s'", expectOutput, curOutput)
+    }
 
 }
